cf20b10caac6d82fb8ed708114cd58e9cf7f209d
sbonada <santiago.bonada@uoit.net>
2017-02-14 15:57:00 -0500

    makefileified

--- C/cvector/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# not actually completely valid ansi because of mixed declarations, so requires extensions which many compilers support
-CC=gcc
-CFLAGS="-ansi -Wall"
-EXE=vectest
-LDFLAGS="-o "$EXE
-SOURCES="cvector.c vectest.c"
-$CC $CFLAGS $SOURCES $LDFLAGS
